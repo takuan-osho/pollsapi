7c0d1559d041d4ee408297b63d4c5e74b2ea1c79
SHIMIZU Taku <shimizu.taku@gmail.com>
2018-06-11 18:27:07 +0900

    Add serializer

--- polls/urls.py
+++ polls/urls.py
@@ -1,3 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.urls import path
+from .views import polls_list, polls_detail
 
-urlpatterns = []
+urlpatterns = [
+    path("polls/", polls_list, name="polls_list"),
+    path("polls/<int:pk>/", polls_detail, name="polls_detail")
+]
