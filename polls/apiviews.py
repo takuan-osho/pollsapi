cffd2fcbc7477c98e49d10ec1ce3a6d07b4b81f0
SHIMIZU Taku <shimizu.taku@gmail.com>
2018-06-14 18:45:41 +0900

    Add ACL

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.exceptions import PermissionDenied
+
 from django.contrib.auth import authenticate
 
 from .models import Poll, Choice
@@ -13,12 +15,23 @@ class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs['pk'])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
 
 class ChoiseList(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
         return queryset
     serializer_class = ChoiceSerializer
+
+    def post(self, request, *args, **kwargs):
+        poll = Poll.objects.get(pk=self.kwargs['pk'])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return super().post(request, *args, **kwargs)
 
 
 class CreateVote(APIView):
@@ -36,4 +49,19 @@ class CreateVote(APIView):
 
 
 class UserCreate(generics.CreateAPIView):
+    authentication_classes = ()
+    permission_classes = ()
     serializer_class = UserSerializer
+
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({'token': user.auth_token.key})
+        else:
+            return Response({'error': 'Wrong condition'}, status=status.HTTP_400_BAD_REQUEST)
--- polls/urls.py
+++ polls/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from .apiviews import PollViewSet, ChoiseList, CreateVote, UserCreate
+from .apiviews import PollViewSet, ChoiseList, CreateVote, UserCreate, LoginView
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, base_name='polls')
@@ -10,7 +10,8 @@ router.register('polls', PollViewSet, base_name='polls')
 urlpatterns = [
     path("polls/<int:pk>/choices/", ChoiseList.as_view(), name="choice_list"),
     path("polls/<int:pk>/choices/<int:choice_pk>/vote/", CreateVote.as_view(), name="create_vote"),
-    path("users/", UserCreate.as_view(), name="user_create")
+    path("users/", UserCreate.as_view(), name="user_create"),
+    path("login/", LoginView.as_view(), name="login"),
 ]
 
 urlpatterns += router.urls
